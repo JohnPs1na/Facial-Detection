e594ecf0029f37b486733d4cfc55cc4e4424d2e6
dionisie hadirca <dionisie.hadirca@s.unibuc.ro>
2023-02-07 22:35:19 +0200

    added project 2

---
+++ sauce/Parameters.py
@@ -0,0 +1,21 @@
+import os
+
+class Parameters:
+    def __init__(self):
+        self.base_dir = '../data'
+        self.dir_pos_examples = "../data/exemplePozitive"
+        self.dir_neg_examples = "../data/exempleNegative"
+        self.dir_test_examples = ""  # 'exempleTest/CursVA'   'exempleTest/CMU+MIT'
+        self.path_annotations = os.path.join(self.base_dir, 'validare/task1_gt_validare.txt')
+        self.dir_save_files = os.path.join(self.base_dir, 'salveazaFisiere')
+
+        # set the parameters
+        self.dim_window = 72  # exemplele pozitive (fete de oameni cropate) au 36x36 pixeli
+        self.dim_hog_cell = 6  # dimensiunea celulei
+        self.dim_descriptor_cell = 36  # dimensiunea descriptorului unei celule
+        self.overlap = 0.3
+        self.number_positive_examples = 7236  # numarul exemplelor pozitive
+        self.number_negative_examples = 10000  # numarul exemplelor negative
+        self.overlap = 0.3
+        self.has_annotations = False
+        self.threshold = 0
